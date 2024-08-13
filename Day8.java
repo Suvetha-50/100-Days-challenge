a9d712f5f268560277f292eccb95598d713a5100
Suvetha T <149592628+Suvetha-50@users.noreply.github.com>
2024-08-13 23:10:43 +0530

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,15 @@
+//Star Pattern
+import java.util.Scanner;
+class Day8{
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
