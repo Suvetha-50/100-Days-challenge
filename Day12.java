ddc7e60c6c7c9bd573fb3b280ddacff4c387e79c
Suvetha T <149592628+Suvetha-50@users.noreply.github.com>
2024-08-17 21:14:30 +0530

    Add files via upload

---
+++ Day12.java
@@ -0,0 +1,11 @@
+//power of a number
+import java.util.*;
+import java.util.Scanner;
+class Day12{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double n= sc.nextInt();
+        System.out.println(Math.pow(a,n));
+    }
+}
