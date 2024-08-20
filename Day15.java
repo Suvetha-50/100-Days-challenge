8077072d45ba0695d8038468acfcdff736f93fc4
Suvetha T <149592628+Suvetha-50@users.noreply.github.com>
2024-08-20 22:54:09 +0530

    Add files via upload

---
+++ Day15.java
@@ -0,0 +1,21 @@
+//abundant number
+import java.util.Scanner;
+
+class Day15{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0) {
+                sum += i;
+            }
+        }
+        if(sum>n){
+            System.out.println("It is an abundant number with abundance value "+(sum-n));
+        }
+        else{
+            System.out.println("Not a abundant number");
+        }
+    }
+}
