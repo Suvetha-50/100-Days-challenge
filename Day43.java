2b8af0a6c8c9b19c12cf1b12459cfaaf461886f4
Suvetha T <149592628+Suvetha-50@users.noreply.github.com>
2024-09-17 23:12:35 +0530

    Update Day43.java

--- Day43.java
+++ Day43.java
@@ -1,1 +1,9 @@
-
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        long nLong = (long)n;
+        System.out.print(nLong * (nLong + 1) / 2);
+    }
+}
